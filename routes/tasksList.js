8ac5e6891c38b977908712a3710471294087c660
DawidWebb <tomasz.matras@developerweb.pl>
2021-06-11 13:26:39 +0200

    feat: add taskList routes and controllers

---
+++ routes/tasksList.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const tasksList = require("../controllers/tasksList");
+
+const router = express.Router();
+
+router.get("/:id", tasksList.getList);
+router.get("/", tasksList.getLists);
+router.post("/", tasksList.postList);
+router.put("/", tasksList.putList);
+router.delete("/:id", tasksList.deleteList);
+router.use((request, response) => response.status(404).end());
+
+module.exports = router;
