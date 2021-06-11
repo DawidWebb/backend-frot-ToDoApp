ce200358b77219cf4f1d934f985ae54d271e37c6
DawidWebb <tomasz.matras@developerweb.pl>
2021-06-11 13:25:34 +0200

    first commit: setup project, add server component

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const tasksListRoutes = require("./routes/tasksList");
+const usersRoutes = require("./routes/users");
+
+const server = express();
+
+server.use(bodyParser.json());
+server.use(cors());
+
+server.use("/tasks-list", tasksListRoutes);
+server.use("/users", usersRoutes);
+
+server.listen(3001, () => console.log("Server for ToDoApp is started..."));
